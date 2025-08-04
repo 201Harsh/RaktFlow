ed49783d9d5a4ee5b02929226653248519a8ba36
Harsh <gamerpandeyharsh@gmail.com>
2025-08-04 22:21:17 +0530

    add chat UI

---
+++ Frontend/src/Pages/RaktFlowChat.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RaktFlowChat = () => {
+  return (
+    <div>RaktFlowChat</div>
+  )
+}
+
+export default RaktFlowChat
--- Frontend/src/Routes/Router.jsx
+++ Frontend/src/Routes/Router.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "../Pages/Register";
 import Login from "../Pages/Login";
+import RaktFlowChat from "../Pages/RaktFlowChat";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login />} />.
+        <Route path="/chat" element={<RaktFlowChat/>} />.
       </Routes>
     </BrowserRouter>
   );
