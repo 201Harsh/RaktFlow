3687f498bd7cbfcb08736f8fc10afff55021cce0
Harsh <gamerpandeyharsh@gmail.com>
2025-08-05 16:12:46 +0530

    cors setup

--- Backend/app.js
+++ Backend/app.js
@@ -10,7 +10,12 @@ const cors = require("cors");
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 
 app.use("/users", UserRouter);
 
