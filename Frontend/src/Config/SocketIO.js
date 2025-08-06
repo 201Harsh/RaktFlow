faa1908b3cf17c53017b897bb38ff59d3f06746d
Harsh <gamerpandeyharsh@gmail.com>
2025-08-06 14:07:00 +0530

    soketIo logic

--- Frontend/src/Config/SocketIO.js
+++ Frontend/src/Config/SocketIO.js
@@ -2,6 +2,7 @@ import { io } from "socket.io-client";
 
 const socket = io(import.meta.env.VITE_BACKEND_URL, {
   transports: ["websocket"], // Optional: ensure WebSocket is used
+  withCredentials: true,
 });
 
 socket.on("connect", () => {
